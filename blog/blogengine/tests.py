d3fafb8df06db4fcec4cc7bf4803b60f77d41367
Alexander Schneider <alexander@schneider.gg>
2018-08-31 15:20:00 +0200

    Add Post model to blogengine

--- blog/blogengine/tests.py
+++ blog/blogengine/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.utils import timezone
+from blogengine.models import Post
 
-# Create your tests here.
+class PostTest(TestCase):
+    def test_create_post(self):
+        post = Post()
+        post.title = "Test post"
+        post.text = "Hello World!"
+        post.pub_date = timezone.now()
+        post.save()
+
+        all_posts = Post.objects.all()
+        self.assertEquals(len(all_posts), 1)
+        only_post = all_posts.first()
+        self.assertEquals(only_post, post)
+
+        self.assertEquals(only_post.title, "Test post")
+        self.assertEquals(only_post.text, "Hello World!")
