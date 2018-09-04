97ca06c5e8bb1eff29f7a7ee63ecaae75039c03d
Alexander Schneider <alexander@schneider.gg>
2018-09-04 16:35:14 +0200

    Add pagination

--- blog/blogengine/views.py
+++ blog/blogengine/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from blogengine.models import Post
+from django.core.paginator import Paginator
 
 # Create your views here.
 def all_posts(request):
-    return render(request, 'blogengine/post_overview.html', {'posts': Post.objects.all()})
+    page = request.GET.get("page")
+    paginator = Paginator(Post.objects.all(), 5)
+    posts = paginator.get_page(page)
+    return render(request, 'blogengine/post_overview.html', {'posts': posts})
